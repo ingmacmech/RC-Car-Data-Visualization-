a65eea86c872ec44f6c9fd3ba333af982d7a2a83
Marcus <marcus.schluep@kns.ch>
2017-09-24 20:20:16 +0200

    Add plots for Spring rate

--- Spring_and_Damper.m
+++ Spring_and_Damper.m
@@ -7,7 +7,7 @@ s = [0 3 6 9 12 15]';
 front_right = [  0    0   0   0   0;
                 142  142 141 142 143;
                 287  284 287 287 287;
-                437  436 463 438 438;
+                437  436 436 438 438;
                 589  592 589 591 591;
                 749  748 747 750 751]/1000 * 9.81;
           
@@ -33,47 +33,93 @@ rear_right = [ 0   0   0   0   0;
               477 477 476 480 476 ]/1000 * 9.81;
           
 %% Regression
+mean_front_left = mean(front_left,2);
+mean_front_right = mean(front_right,2);
+mean_rear_left = mean(rear_left,2);
+mean_rear_right = mean(rear_right,2);
 
-
+f_front_left = fitlm(s,mean_front_left,'linear','Intercept',false);
+f_front_right = fitlm(s,mean_front_right,'linear','Intercept',false);
+f_rear_left = fitlm(s,mean_rear_left,'linear','Intercept',false);
+f_rear_right = fitlm(s,mean_rear_right,'linear','Intercept',false);
 
 %% Plot Data
 
-figure()
+figure('units','normalized','outerposition',[0 0 1 1])
 subplot(2,2,1)
 hold on
-plot(s, front_left(:,1), '+')
-plot(s, front_left(:,2), '+')
-plot(s, front_left(:,3), '+')
-plot(s, front_left(:,4), '+')
-plot(s, front_left(:,5), '+')
+plot(s, front_left(:,1), '+b','HandleVisibility','off')
+plot(s, front_left(:,2), '+b','HandleVisibility','off')
+plot(s, front_left(:,3), '+b','HandleVisibility','off')
+plot(s, front_left(:,4), '+b','HandleVisibility','off')
+plot(s, front_left(:,5), '+b')
+plot(s, f_front_left.Fitted,'--r')
+text(5,2,['Federkonstannte [k] = ',...
+                            num2str(f_front_left.Coefficients.Estimate) ,'$\frac{N}{mm}$'],...
+                           'HorizontalAlignment','left','interpreter','latex');
 hold off
+grid minor
+ylabel('Kraft (N)')
+xlabel('Weg (mm)')
+title('Federkennlinie vorne links')
+legend('Data','Fit','Location','southeast')
 
 
 subplot(2,2,2)
 hold on
-plot(s, front_right(:,1), '+')
-plot(s, front_right(:,2), '+')
-plot(s, front_right(:,3), '+')
-plot(s, front_right(:,4), '+')
+plot(s, front_right(:,1), '+b','HandleVisibility','off')
+plot(s, front_right(:,2), '+b','HandleVisibility','off')
+plot(s, front_right(:,3), '+b','HandleVisibility','off')
+plot(s, front_right(:,4), '+b','HandleVisibility','off')
 plot(s, front_right(:,5), '+')
+plot(s, f_front_right.Fitted,'--r')
 hold off
+grid minor
+ylabel('Kraft (N)')
+xlabel('Weg (mm)')
+title('Federkennlinie vorne rechts')
+legend('Data','Fit','Location','southeast')
 
 subplot(2,2,3)
 hold on
-plot(s, rear_left(:,1), '+')
-plot(s, rear_left(:,2), '+')
-plot(s, rear_left(:,3), '+')
-plot(s, rear_left(:,4), '+')
-plot(s, rear_left(:,5), '+')
+plot(s, rear_left(:,1), '+b','HandleVisibility','off')
+plot(s, rear_left(:,2), '+b','HandleVisibility','off')
+plot(s, rear_left(:,3), '+b','HandleVisibility','off')
+plot(s, rear_left(:,4), '+b','HandleVisibility','off')
+plot(s, rear_left(:,5), '+b')
+plot(s, f_rear_left.Fitted,'--r')
 hold off
+grid minor
+ylabel('Kraft (N)')
+xlabel('Weg (mm)')
+title('Federkennlinie hinten links')
+legend('Data','Fit','Location','southeast')
 
 
 subplot(2,2,4)
 hold on
-plot(s, rear_right(:,1), '+')
-plot(s, rear_right(:,2), '+')
-plot(s, rear_right(:,3), '+')
-plot(s, rear_right(:,4), '+')
-plot(s, rear_right(:,5), '+')
+plot(s, rear_right(:,1), '+b','HandleVisibility','off')
+plot(s, rear_right(:,2), '+b','HandleVisibility','off')
+plot(s, rear_right(:,3), '+b','HandleVisibility','off')
+plot(s, rear_right(:,4), '+b','HandleVisibility','off')
+plot(s, rear_right(:,5), '+b')
+plot(s, f_rear_right.Fitted,'--r')
 hold off
+grid minor
+ylabel('Kraft (N)')
+xlabel('Weg (mm)')
+title('Federkennlinie hinten rechts')
+legend('Data','Fit','Location','southeast')
 
+figure('units','normalized','outerposition',[0 0 1 1])
+hold on
+plot(s,f_front_left.Fitted)
+plot(s,f_front_right.Fitted)
+plot(s,f_rear_left.Fitted)
+plot(s,f_rear_right.Fitted)
+hold off
+grid minor
+title('Vergleich Federkennlinien')
+ylabel('Kraft (N)')
+xlabel('Weg (mm)')
+legend('FL','FR','HL','HR','Location','southeast')
