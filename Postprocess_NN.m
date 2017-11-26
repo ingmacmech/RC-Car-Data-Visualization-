857f718ac1f474975dcdb42a563a1bdfbdf5130a
Marcus <marcus.schluep@kns.ch>
2017-11-26 15:19:05 +0100

    Add a plot to postprocess to schow only test data

--- Postprocess_NN.m
+++ Postprocess_NN.m
@@ -14,3 +14,26 @@ ylabel('Pitch angle (°)')
 legend('estimation','measured');
 grid on
 grid minor
+
+for n = 1 : size(testData.StartStopFeautres,1)
+    
+    start = testData.StartStopFeautres(n,1);
+    stop = testData.StartStopFeautres(n,1);
+    
+    figure('units','normalized','outerposition',[0 0 1 1])
+
+    hold on
+    plot(testData.OutputFeautures(start:stop,1))
+    plot(nn_output(start:stop,1))
+    plot([1, size(testData.OutputFeautures(start:stop,1))],...
+         [mean(testData.OutputFeautures(start:stop,1)) ,...
+          mean(testData.OutputFeautures(start:stop,1))], '--r')
+    plot([1, size(nn_output(start:stop,1))],...
+         [mean(nn_output(start:stop,1)) ,...
+          mean(nn_output(start:stop,1))], '--r')
+   
+    hold off
+    
+    
+end
+
